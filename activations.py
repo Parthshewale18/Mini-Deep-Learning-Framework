b068cacfa3ef4a6db9fed25a021744c905eafad1
Parth Shewale <shewaleparth19@gmail.com>
2026-09-21 19:12:48 +0530

    added leaky_relu, elu, selu activation function

--- activations.py
+++ activations.py
@@ -26,11 +26,49 @@ def tanh(x):
 
     return out
 
+# ReLU
 def relu(x):
     out = Tensor(max(0, x.data), (x,), 'relu')
 
     def _backward():
-        x.grad += (out.data > 0) * out.grad
+        x.grad += (1.0 if x.data > 0 else 0.0) * out.grad
 
     out._backward = _backward
+
     return out
+
+# Leaky ReLU
+def leaky_relu(x, alpha=0.01):
+    out = Tensor(x.data if x.data > 0 else alpha * x.data, (x,), 'leaky_rely')
+
+    def _backward():
+        x.grad += (1.0 if x.data > 0 else alpha) * out.grad
+
+    out._backward = _backward
+
+    return out
+
+# ELU
+def elu(x, alpha=1.0):
+    out = Tensor(x.data if x.data > 0 else alpha * (math.exp(x.data) - 1), (x,), 'elu')
+
+    def _backward():
+        x.gard = (1.0 if x.data > 0 else alpha * math.exp(x.data)) * out.grad
+
+    out._backward = _backward
+
+    return out
+
+def selu(x):
+    alpha = 1.6732632423543772
+    scale = 1.0507009873554805
+
+    out = Tensor(scale * (x.data if x.data > 0 else alpha * (math.exp(x.data) - 1)), (x,), 'selu')
+
+    def _backward():
+        x.grad += (scale * (1.0 if x.data > 0 else alpha * math.exp(x.data))) * out.grad
+
+    out._backward = _backward
+
+    return out
+
